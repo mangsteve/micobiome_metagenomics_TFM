f49d63fcfde015fb9160be6b66c10fe3f4c2836a
Carlos Mora Martínez <carmoma9@gmail.com>
2023-09-11 15:17:35 +0100

    Added -y to conda commands for non interactive installs

--- scripts2/create_environments.sh
+++ scripts2/create_environments.sh
@@ -5,62 +5,62 @@ conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 
-conda create --name fastqc-env python=3.10
+conda create -y --name fastqc-env python=3.10
 conda activate fastqc-env
-conda install -c bioconda fastqc
+conda install -y -c bioconda fastqc
 
-conda create --name trimmomatic-env python=3.10
+conda create -y --name trimmomatic-env python=3.10
 conda activate trimmomatic-env
-conda install -c bioconda trimmomatic
+conda install -y -c bioconda trimmomatic
 
-conda create --name bowtie2-env python=3.10
+conda create -y --name bowtie2-env python=3.10
 conda activate bowtie2-env
-conda install -c bioconda bowtie2 samtools
+conda install -y -c bioconda bowtie2 samtools
 
-conda create --name bwa-env python=3.10
+conda create -y --name bwa-env python=3.10
 conda activate bwa-env
-conda install -c bioconda bwa bwa-mem2 samtools
+conda install -y -c bioconda bwa bwa-mem2 samtools
 
-#conda create --name kraken-env python=3.10
+#conda create -y --name kraken-env python=3.10
 #conda activate kraken-env
-#conda install -c bioconda kraken 
-#conda install -c bioconda krakentools 
+#conda install -y -c bioconda kraken 
+#conda install -y -c bioconda krakentools 
 
-conda create --name kraken2-env python=3.10
+conda create -y --name kraken2-env python=3.10
 conda activate kraken2-env
-conda install -c bioconda kraken2
-conda install -c bioconda pigz 
+conda install -y -c bioconda kraken2
+conda install -y -c bioconda pigz 
 
-conda create --name bracken-env python=3.10
+conda create -y --name bracken-env python=3.10
 conda activate bracken-env
-conda install -c bioconda bracken
+conda install -y -c bioconda bracken
 
-conda create --name krona-env python=3.10
+conda create -y --name krona-env python=3.10
 conda activate krona-env
-conda install -c bioconda krona
+conda install -y -c bioconda krona
 
-conda create --name multiqc-env python=3.10
+conda create -y --name multiqc-env python=3.10
 conda activate multiqc-env
-conda install -c bioconda multiqc
+conda install -y -c bioconda multiqc
 
-conda create --name seqtk-env python=3.10
+conda create -y --name seqtk-env python=3.10
 conda activate seqtk-env
-conda install -c bioconda seqtk pigz
+conda install -y -c bioconda seqtk pigz
 
-conda create --name seqkit-env python=3.10
+conda create -y --name seqkit-env python=3.10
 conda activate seqkit-env
-conda install -c bioconda seqkit pigz
+conda install -y -c bioconda seqkit pigz
 
-conda create --name humann-env python=3.10
+conda create -y --name humann-env python=3.10
 conda activate humann-env
-conda install -c bioconda humann
+conda install -y -c bioconda humann
 
 ## Add Krakentools to kraken2-env
 #get scripts from github
 mkdir tmp
 cd tmp
 conda activate kraken2-env
-conda install git
+conda install -y git
 git clone https://github.com/jenniferlu717/KrakenToolsls
 
 #get conda bin directory
@@ -75,3 +75,31 @@ chmod 777 $condadir/*.py
 #Clean temporary directory
 cd ../
 rm -rf tmp
+
+
+##Initialize Krona env
+conda activate krona_env
+
+# delete a symbolic link that is not correct
+rm -rf /home/carmoma/anaconda3//envs/krona_env/opt/krona/taxonomy
+
+#  create -y a directory in  home where the krona database will live
+mkdir -p ~/krona/taxonomy
+
+# now we make a symbolic link to that directory
+ln -s ~/krona/taxonomy /home/carmoma/anaconda3/envs/krona_env/opt/krona/taxonomy
+
+# the krona downloader fails when downloading the latest NCBI db
+# ktUpdateTaxonomy.sh ~/krona/taxonomy
+# taxdump.tar.gz needs to be downloaded manually from ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz 
+# taxdump.tar.gz needs to be placed in ~/krona/taxonomy
+# then one needs to run  ktUpdateTaxonomy.sh --only-build
+ktUpdateTaxonomy.sh --only-build
+
+cd ~/projects/pollen/results/problem_samples_1/kraken_taxons
+mkdir krona_charts
+for i in $(ls *txt.gz); do echo $i; ktImportTaxonomy -m 3 -t 5 <(zcat $i) -o krona_charts/${i%.txt.gz}.html; done
+ktImportTaxonomy -m 3 -t 5 <(zcat *.txt.gz) -o krona_charts/all_mock.html
+
+cd ~/projects/pollen/results/mock_kraken1/kraken_tx_ont
+cd ~/projects/pollen/results/mock_kraken1/kraken_tx_illumina
