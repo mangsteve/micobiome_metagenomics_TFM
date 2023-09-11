be05f3204639bc84b06be03181f732b87c008af6
Carlos Mora Martínez <carmoma9@gmail.com>
2023-09-11 17:21:45 +0100

    changed conda module load

--- sbatch/sbatch_garnatxa.sh
+++ sbatch/sbatch_garnatxa.sh
@@ -20,7 +20,7 @@
 #SBATCH --cpus-per-task 4
 #SBATCH --mem=2G
 
-module load  anaconda
+module load anaconda3_2022.10
 conda activate nextflow-env
 
 nextflow run all.nf -c config/run_samples_cluster.config -profile conda -resume -with-report report.html -with-dag pipeline_dag.html
