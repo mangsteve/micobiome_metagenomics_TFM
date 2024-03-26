10ffdf4983453496986fa70302310ebad9cbc8ca
Carlos Mora Martínez <carmoma9@gmail.com>
2024-03-26 18:13:40 +0100

    Added commands to copy images to server

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -13,3 +13,11 @@ sudo docker run -ti --entrypoint /bin/bash ccarlos/registry:kraken_with_pigz
 # Enter and mount your home to the container
 sudo docker run -ti --mount type=bind,source=/home,target=/home  --entrypoint /bin/bash nanozoo/krona
 
+# Save images in order to transfer to server
+sudo docker save -o dockerimage_kraken_with_pigz.tar ccarlos/registry:kraken_with_pigz 
+sudo docker save -o dockerimage_seqkit_with_samtools.tar ccarlos/registry:seqkit_with_samtools
+
+# Transfer .tar files via scp and go to the server
+cd /DATA12/COMUN/tmp
+sudo docker load -i dockerimage_kraken_with_pigz.tar
+sudo docker load -i dockerimage_seqkit_with_samtools.tar
